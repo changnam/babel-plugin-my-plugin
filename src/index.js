17a52c62d8f553ff8abdf0183aaf5e5ca5796f39
Changnam Go <changnam.go@gmail.com>
2025-02-12 20:07:41 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+export default function(babel) {
+    return {
+        visitor: {
+            Identifier(path, state) {
+                const name = path.node.name;
+                // console.log(state.file.opts.filename + "==> " + name.split("").reverse().join(""));
+            },
+            FunctionDeclaration(path,state) {
+                const filename = state.file.opts.filename.replace(/\\/g,"\\\\");
+                if(!filename.includes("node_modules")){
+                    console.log(state.file.opts.filename + "==> Enter "+path.node.id.name);
+                    
+                    const codeStart = `console.log("${filename} ${path.node.id.name} started.");`;
+                    path.get('body').unshiftContainer('body',babel.parse(codeStart).program);
+
+                    const codeEnd = `console.log("${filename} ${path.node.id.name} ended.");`;
+					
+					const blockStatement = path.get('body')
+					const lastExpression = blockStatement.get('body').pop();
+					const timeEndStatement = babel.parse(codeEnd).program;
+
+                    if (lastExpression.type !== 'ReturnStatement') {
+						lastExpression.insertAfter(timeEndStatement);
+					} else {
+						lastExpression.insertBefore(timeEndStatement);
+					}	
+
+                    path.skip();
+
+                }
+            }
+        }
+    }
+}
